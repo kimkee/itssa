b7fcb65e001af6a10e803e3741866365cef64ad1
김기현 <kimkee7@gmail.com>
2025-03-19 15:39:01 +0900

    Update itssa.js

--- itssa.js
+++ itssa.js
@@ -1,3 +1,15 @@
 document.querySelectorAll('.xe_content img, .rhymix_content img').forEach(img=>{
     img.addEventListener('click',e => img.classList.toggle('size'))
 })
+
+document.querySelectorAll('.list-body .item-info .writer a').forEach(els => {
+	const blockUser = [
+		'member_11567789', // 어그로
+		'member_12345678', // 테스트
+	];
+
+	// blockUser 배열에 해당 클래스가 포함되어 있는지 확인
+	if (blockUser.some(blockedClass => els.classList.contains(blockedClass))) {
+		els.closest('li.item').style.display = 'none'; // li.item 숨기기
+	}
+});
