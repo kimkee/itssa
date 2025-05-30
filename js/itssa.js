1ef6c3e5ecac6777daa8391f59424ddb25689135
KimKeeHyun <kimkee7@gmail.com>
2025-05-30 16:35:42 +0900

    0.0.4.1

--- js/itssa.js
+++ js/itssa.js
@@ -13,30 +13,6 @@ document.querySelectorAll('.list-body .item-info .writer a').forEach(els => {
 });
 
 
-// 웹페이지에 DOM 이벤트 리스너 추가
-/* document.addEventListener('click', (event) => {
-    if (event.target && event.target.id === 'saveButton') { // 버튼 ID 확인
-
-    }
-
-	const dataToSave = {
-		key: 'exampleValue',
-		timestamp: new Date().toISOString()
-	};
-
-	// chrome.storage.sync에 데이터 저장
-	chrome.storage.sync.set(dataToSave, () => {
-		console.log('데이터가 저장되었습니다:', dataToSave);
-	});
-}); */
-
-
-
-/* chrome.storage.sync.get(['popkey'], (result) => {
-	console.log('팝업에서 저장된 데이터:', result.popkey);
-	document.querySelector('.logo').textContent = result.popkey;
-}); */
-
 
 const itssaUI = {
 	init: function() {
